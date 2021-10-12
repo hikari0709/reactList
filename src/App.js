6ad1137728695386dc1c99276db484d06ef2625e
hikari <tanimoto0709@gmail.com>
2021-10-13 07:29:48 +0900

    update: TODOの表示とステータスによるリストの出しわけ

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const todoDataUrl = 'http://localhost:3100/todos';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(todoDataUrl);
+      setTodoList(response.data);
+    };
+    fetchData();
+  }, []);
+
+  const inCompletedList = todoList.filter((todo) => {
+    return !todo.done;
+  });
+
+  const completedList = todoList.filter((todo) => {
+    return todo.done;
+  });
+
   return (
-    <p></p>
+    <>
+      <h1>TODO List</h1>
+
+      <h2>未完了TODO</h2>
+      <ul>
+        {inCompletedList.map((todo) => (
+          <li key={todo.id}>
+            {todo.content}({todo.done ? '完了' : '未完了'})
+          </li>
+        ))}
+      </ul>
+
+      <h2>完了TODO</h2>
+      <ul>
+        {completedList.map((todo) => (
+          <li key={todo.id}>
+            {todo.content}({todo.done ? '完了' : '未完了'})
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
