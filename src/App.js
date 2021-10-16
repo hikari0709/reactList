ae5b8004741ee480c6c24685687d6e641a068dec
hikari <tanimoto0709@gmail.com>
2021-10-16 23:51:06 +0900

    add: TODOの追加ボタンの設置とそれに伴う処理を追記

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-const todoDataUrl = 'http://localhost:3100/todos';
+import React, { useState, useEffect, useRef } from 'react';
+import { useTodo } from './hooks/useTodo';
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get(todoDataUrl);
-      setTodoList(response.data);
-    };
-    fetchData();
-  }, []);
+  const { todoList, addTodo } = useTodo();
+
+  const inputElement = useRef(null);
+
+  const handleAddTodoListItem = () => {
+    if (inputElement.current.value === '') return;
+    addTodo(inputElement.current.value);
+    inputElement.current.value = '';
+  };
 
   const inCompletedList = todoList.filter((todo) => {
     return !todo.done;
@@ -25,6 +24,9 @@ function App() {
   return (
     <>
       <h1>TODO List</h1>
+
+      <textarea ref={inputElement} />
+      <button onClick={handleAddTodoListItem}>TODOを追加する</button>
 
       <h2>未完了TODO</h2>
       <ul>
